fe3fe6ea649e8d3e42856ca5239b53b53f296df6
chris-kng <chriskng@stud.ntnu.no>
2024-04-24 14:35:41 +0200

    more testing for intro scene

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,TextPlugin);
-
   let tl = gsap.timeline({
     scrollTrigger: {
-        trigger: ".scene",
+        trigger: ".scene--1",
         pin: true,
         start: "top top",
-        end: "bottom top",
+        end: "+=" + (window.innerHeight * 5),
         scrub: 1,
         snap: {
             snapTo: "labels",
@@ -17,7 +16,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   tl.addLabel("start")
   .add([
-    gsap.from("#patch2-right", {xPercent:30}),
-    gsap.from("#patch2-left", {xPercent:-30})
-  ], "patch");
+    gsap.from(".intro__headline", { autoAlpha: 0 }),
+    gsap.from("#patch2-right", {xPercent:50}),
+    gsap.from("#patch2-left", {xPercent:-50})
+  ], "patch")
+  .add("headline")
+  .to(".intro__headline", { duration: 0.2, opacity: 0 })
+  .add("deer")
+  .fromTo("#deer", {xPercent: 0}, {xPercent: 10*80}); // didn't scale with the width change of the svg
  });
