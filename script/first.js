235e1005f1285db358e1a239196b099f9f2c441b
chris-kng <chriskng@stud.ntnu.no>
2024-04-28 04:00:17 +0200

    sort .js file

--- script/first.js
+++ script/first.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable);
 
+  // based on the 'Advanced' example within the GSAP documentaion page on ScrollTrigger: https://gsap.com/docs/v3/Plugins/ScrollTrigger/
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".scene__one",
@@ -24,19 +25,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
       gsap.from(".forest-intro__patch-two--right", { xPercent: 60 }),
       gsap.from(".forest-intro__patch-two--left", { xPercent: -60 })
     ], "sidePatch")
-    .add("headline")
     .to(".intro__headline", { duration: 0.2, opacity: 0 })
-    .add("deer")
     .from("#deer", { xPercent: -100 * 10, duration: 3 })
     .add([
       gsap.to("#gas-forest", { opacity: 1 }),
       gsap.from(".scene__fact-box--forest", { yPercent: 300, duration: 5 }) 
     ], "factForest")
-    .add("removeFactForest")
     .to(".scene__fact-box--forest", { yPercent: 300, duration: 5 })
-    .add("removeGas")
     .to("#gas-forest", { opacity: 0 })
-    .add("bulldozer")
     .to(".bulldozer", {
       keyframes: {
         xPercent: [-100, 300],
@@ -46,14 +42,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 });
 
-// to reset the ScrollTrigger on page reload
-window.addEventListener("beforeunload", function (e) {
-  ScrollTrigger.getAll().forEach(trigger => {
-    trigger.scroll(progress => progress(0));
-  });
-});
-
 // not wanting animations to loop when not necessary
+// using Intersection Observer for the first scene to pause animations if not intersecting (not visible on the screen)
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
 
