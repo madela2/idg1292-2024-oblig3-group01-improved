f3d991a55b902d24a489caa6cc73e4e7182b03c7
chris-kng <chriskng@stud.ntnu.no>
2024-04-30 15:44:13 +0200

    commit what I have till now for fixing

--- script/first.js
+++ script/first.js
@@ -1,7 +1,6 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, Draggable);
 
-  // based on the 'Advanced' example within the GSAP documentaion page on ScrollTrigger: https://gsap.com/docs/v3/Plugins/ScrollTrigger/
   let tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".scene__one",
@@ -13,6 +12,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
         snapTo: "labels",
         duration: { min: 0.2, max: 3 },
         ease: "power1.in"
+      },
+      onComplete: () => {
+        tl.reverse(false);
       }
     }
   });
@@ -29,26 +31,29 @@ document.addEventListener("DOMContentLoaded", (event) => {
     .from("#deer", { xPercent: -100 * 10, duration: 3 })
     .add([
       gsap.to("#gas-forest", { opacity: 1 }),
-      gsap.from(".scene__fact-box--forest", { yPercent: 300, duration: 5 }) 
+      gsap.from(".scene__fact-box--one", { yPercent: 300, duration: 5 })
     ], "factForest")
-    .to(".scene__fact-box--forest", { yPercent: 300, duration: 5 })
+    .to(".scene__fact-box--one", { yPercent: 300, duration: 5 })
     .to("#gas-forest", { opacity: 0 })
+    .add(() => {
+      document.querySelector("#forest-intro").classList.add('shake-element');
+      document.querySelector(".forest-intro__trees").classList.add('shake-element');
+    }, "forestShake")
     .to(".bulldozer", {
       keyframes: {
         xPercent: [-100, 300],
         ease: "power1.inOut"
       },
       duration: 4
-    });
+    })
+    .from(".scene__fact-box--two", { yPercent: 300, duration: 5 })
+    .to(".scene__fact-box--two", { yPercent: 300, duration: 5 });
 });
 
-// not wanting animations to loop when not necessary
-// using Intersection Observer for the first scene to pause animations if not intersecting (not visible on the screen)
+// Intersection Observer for pausing animations when not visible
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
-
     const animationState = entry.isIntersecting ? 'running' : 'paused';
-
     entry.target.querySelector('.leg-set1').style.animationPlayState = animationState;
     entry.target.querySelector('.leg-set2').style.animationPlayState = animationState;
     entry.target.querySelector('#scene__gas-forest').style.animationPlayState = animationState;
@@ -56,5 +61,4 @@ const observer = new IntersectionObserver(entries => {
 });
 
 const sceneOneElement = document.querySelector('.scene__one');
-
 observer.observe(sceneOneElement);
