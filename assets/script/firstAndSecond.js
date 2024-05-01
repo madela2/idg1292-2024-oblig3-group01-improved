308952c1f866b874434ce7ba26c6a086be41663c
anon-IOO-stud-37 <isak_hal@hotmail.com>
2024-05-01 17:29:40 +0200

    disabled some animations due to them not being necessary and causing a lot of gpu usage. Fixed so svg's from first scene will be appended/removed when relevant for lower cpu usage

--- assets/script/firstAndSecond.js
+++ assets/script/firstAndSecond.js
@@ -3,6 +3,7 @@ const windows = document.querySelectorAll('.scene__window');
 const scene = document.querySelector('.scene');
 const secondScene = document.querySelector('.scene__second');
 const trees = document.querySelectorAll('.forest__tree');
+const treesSvg = document.querySelector('.forest__trees');
 const sceneOne = document.querySelector('.scene__one');
 const bulldozer = document.querySelector('.bulldozer__svg');
 const patches = document.querySelectorAll('.forest__patch');
@@ -10,6 +11,10 @@ const leftPatchVal = document.querySelector('.forest__patch--left');
 const parOne = document.querySelector('.scene__paragraph--one');
 const parTwo = document.querySelector('.scene__paragraph--two');
 const headline = document.querySelector('.scene__headline');
+const buildingTwo = document.querySelector('#scene__building--two');
+const forestSvg = document.querySelector('.forest__svg');
+const deer = document.querySelector('.forest__deer');
+const sceneOneSvgs = [forestSvg, bulldozer, treesSvg, deer];
 let activeParOneOpacity = 0, activeParTwoOpacity = 0, forestPatchPosition = 0, paragraphOnePosition = 0,
     paragraphTwoPosition = 0;
 let activeParagraphOne, activeParagraphTwo, secondSceneInitiation = false, secondPhaseThree = false;;
@@ -18,11 +23,12 @@ scene.removeChild(secondScene);
 
 function InitiatesecondSceneCss() {
     //removes and appends second/city-scene
-    scene[secondSceneInitiation ? 'append' : 'removeChild'](secondScene);
+    !scene[secondSceneInitiation ? 'append' : 'removeChild'](secondScene);
     sceneOne.classList[secondSceneInitiation ? 'add' : 'remove']('scene__one--dark');
     patches.forEach(patch => patch.classList[secondSceneInitiation ? 'add' : 'remove']('forest__patch--grey'));
     buildings.forEach(building => building.classList.toggle(building.id));
     windows.forEach(window => window.classList[secondSceneInitiation ? 'add' : 'remove'](window.getAttribute('data-windows')));
+    secondSceneInitiation ? setTimeout(() => buildingTwo.getBoundingClientRect().top / window.innerHeight * 100 === 0 ? sceneOneSvgs.forEach(el => sceneOne.removeChild(el)) : null, 4000) : sceneOne.append(forestSvg, bulldozer, treesSvg, deer);
 }
 
 function sceneOnePhaseTwo(event) {
@@ -31,7 +37,7 @@ function sceneOnePhaseTwo(event) {
     //DONT modify below line: Very hard to figure out! Calculation value used in scss for paragraph opacity.
     activeParOneOpacity = ((((window.innerHeight / window.innerHeight * 100) - (activeParagraphOne.getBoundingClientRect().top / window.innerHeight * 100 + activeParagraphOne.getBoundingClientRect().height / window.innerHeight * 100)) / 120) * 2) * 2;
     //gives value to variables used in scssso position is done in css: To allow for scrolling without scrolling the webpage or having overflow
-    event.deltaY < 0 ? paragraphOnePosition++ : paragraphOnePosition--;
+    !bulldozer.classList.contains('bulldozer__svg--active') ? (event.deltaY < 0 ? paragraphOnePosition++ : paragraphOnePosition--) : null;
     activeParagraphOne.style.setProperty('--paragraph-pos', `${paragraphOnePosition.toString() * 15}px`);
     activeParagraphOne.style.setProperty('--paragraph-opacity', `${activeParOneOpacity.toString()}`);
     headline.style.setProperty('--paragraph-opacity', [headline.getBoundingClientRect().top >= parOne.getBoundingClientRect().top ? 1 : 0]);
